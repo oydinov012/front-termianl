3c5ba52df2fadcaa0544e969fa8e1c01bde82f39
Ilyos <223318977+Ilyos-akang@users.noreply.github.com>
2026-07-02 02:15:01 +0500

    deploy

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL =  'http://localhost:8000';
+const API_BASE_URL =  'https://terminaleditor.duckdns.org/';
 // import.meta.env.VITE_API_URL ||
 
 export const api = axios.create({
